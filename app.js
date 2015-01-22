a2bb543f887a4f08fd3f50bcb4a42f86af227551
alexje2695 <alexje2695@hotmail.com>
2015-01-22 08:55:23 -0500

    jose
    jose

--- app.js
+++ app.js
@@ -34,8 +34,21 @@ router.get('/', function(req, res) {
 
 router.use(function(req, res, next) {  
     res.header('Access-Control-Allow-Origin', '*');
+
+    //--
+     // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.header('Access-Control-Allow-Credentials', true);
+    //--
     next();
 });
+
 
 app.use(router);
 
